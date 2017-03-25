ad2c7a3e0ca937f6a41263b4dab5a1d363a70dd9
Carlos Doki <carlosdoki@Carloss-MacBook-Pro.local>
2017-03-25 16:33:58 -0300

    Modificaçao na interface

---
+++ BlueHack2017/chat.swift
@@ -0,0 +1,9 @@
+//
+//  chat.swift
+//  BlueHack2017
+//
+//  Created by Carlos Doki on 25/03/17.
+//  Copyright © 2017 Carlos Doki. All rights reserved.
+//
+
+import Foundation
---
+++ BlueHack2017/chatCell.swift
@@ -0,0 +1,24 @@
+//
+//  chatCell.swift
+//  BlueHack2017
+//
+//  Created by Carlos Doki on 25/03/17.
+//  Copyright © 2017 Carlos Doki. All rights reserved.
+//
+
+import UIKit
+
+class chatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
